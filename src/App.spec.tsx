a53df07cd17a3443b31cedd086129f527584d614
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-06-07 12:23:10 +0300

    Mock test for Posts List

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,37 @@
 import { render, screen } from "@testing-library/react";
 
 import App from "./App";
+import { PostQuery } from "./models";
+import { useGetPosts } from "./services/posts";
+jest.mock("./services/posts");
 
-test("renders learn react link", () => {
+test("renders posts", () => {
+  const query: PostQuery = {
+    data: [
+      {
+      id: 1,
+      name: "Satheesh",
+      email: "Sat@mail.com",
+      address: "South street"
+    },
+    {
+      id: 2,
+      name: "John",
+      email: "john@mail.com",
+      address: "North street"
+    },
+  ],
+    loading:false,
+  };
+
+  const hook = useGetPosts as jest.Mock;
+  hook.mockReturnValue(query);
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const posts = screen.getAllByText(/john/i);
+  expect(posts.length).toBe(2);
+  posts.forEach((post) => {
+    expect(post).toBeInTheDocument();
+  });
 });
